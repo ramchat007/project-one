ab9f146d58d2ae5cbd6be7f8a0c9b71d7a6352af
ramchat007 <47141980+ramchat007@users.noreply.github.com>
2020-02-24 16:56:55 +0530

    build pages

---
+++ precache-manifest.b8710f69eea124b26281d3eadd3267e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "136de7e3e3e427d45348fe906bfcf133",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d80a794b4ffb68e59273",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "281d221d57386b176d78",
+    "url": "/static/css/main.9354a51f.chunk.css"
+  },
+  {
+    "revision": "d80a794b4ffb68e59273",
+    "url": "/static/js/2.f5f990c5.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.f5f990c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "281d221d57386b176d78",
+    "url": "/static/js/main.9d371edc.chunk.js"
+  },
+  {
+    "revision": "5b28d66fda036d53e53e",
+    "url": "/static/js/runtime-main.a5cc5783.js"
+  }
+]);
