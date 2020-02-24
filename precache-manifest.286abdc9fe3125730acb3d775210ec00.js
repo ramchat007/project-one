89058ecf3ddcefa043d594044ce284a6bd16dbcb
ramchat007 <47141980+ramchat007@users.noreply.github.com>
2020-02-24 17:19:05 +0530

    database

---
+++ precache-manifest.286abdc9fe3125730acb3d775210ec00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cde4eb5e4dc39d94ed29b39e5908a77f",
+    "url": "/project-one/index.html"
+  },
+  {
+    "revision": "d80a794b4ffb68e59273",
+    "url": "/project-one/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "8615d82561343448d247",
+    "url": "/project-one/static/css/main.9354a51f.chunk.css"
+  },
+  {
+    "revision": "d80a794b4ffb68e59273",
+    "url": "/project-one/static/js/2.f5f990c5.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/project-one/static/js/2.f5f990c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8615d82561343448d247",
+    "url": "/project-one/static/js/main.1ab2be4d.chunk.js"
+  },
+  {
+    "revision": "fc91344a3ee408ee0729",
+    "url": "/project-one/static/js/runtime-main.1030f15e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b8710f69eea124b26281d3eadd3267e6.js"
+  "/project-one/precache-manifest.286abdc9fe3125730acb3d775210ec00.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/project-one/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
